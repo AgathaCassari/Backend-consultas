312dca31cf22ec94651c3843e89d88fb3a4d31a1
Agatha Cassari Benedicto <agathacassaribenedicto@MacBook-Air-de-Agatha.local>
2026-03-20 21:18:09 -0300

    feat: implementação buscar por ID

--- src/main/java/com/fiap/ec/backend_consultas/service/EspecialidadeService.java
+++ src/main/java/com/fiap/ec/backend_consultas/service/EspecialidadeService.java
@@ -1,20 +1,31 @@
 package com.fiap.ec.backend_consultas.service;
+
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.fiap.ec.backend_consultas.model.Especialidade;
 import com.fiap.ec.backend_consultas.repository.EspecialidadeRepository;
+
 @Service
 public class EspecialidadeService {
+
     private final EspecialidadeRepository repository;
+
     public EspecialidadeService(EspecialidadeRepository repository) {
         this.repository = repository;
     }
+
     public Especialidade salvar(Especialidade especialidade) {
         return repository.save(especialidade);
     }
+
     public List<Especialidade> listar() {
         return repository.findAll();
+    }
+
+    public Especialidade buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Especialidade não encontrada"));
     }
 }
